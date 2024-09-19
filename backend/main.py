b8f898bb4d6ce60520c6cbac31966abcecb224ea
Adesina Joshua <tolujosh1@gmail.com>
2024-09-19 18:05:44 +0100

    still edit

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,6 @@ from api.v1 import recipes, ingredients, ai_interactions  # Import your new AI r
 app = FastAPI()
 
 # Register API routes
-app.include_router(recipes.router, prefix="/v1")
-app.include_router(ingredients.router, prefix="/v1")
+#app.include_router(recipes.router, prefix="/v1")
+#app.include_router(ingredients.router, prefix="/v1")
 app.include_router(ai_interactions.router, prefix="/v1")
